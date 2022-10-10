5209bbb6444b74b23f6880b987ca261b2ec75723
Vahagn <v.h.v.vahagn@gmail.com>
2022-10-10 21:14:31 +0400

    first commit

---
+++ TestCases/Base_test_file.py
@@ -0,0 +1,12 @@
+import unittest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class BaseTestClass(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.maximize_window()
+        self.driver.delete_all_cookies()
+        self.driver.implicitly_wait(10)
